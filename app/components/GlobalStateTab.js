9d9f2d0ac710f14f9170f88df5df43881b6a81ed
Sean <sean2108@gmail.com>
2018-12-04 01:00:54 +0800

    change variable lists to non typed variable boxes

---
+++ app/components/GlobalStateTab.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import BuildOptions from './BuildOptions';
+import BuildDiagram from './BuildDiagram';
+import BuildTabs from './BuildTabs';
+import Button from '@material-ui/core/Button';
+import StateRow from './GlobalStateObjectRow';
+import VariableList from './VariableList';
+
+const styles = theme => ({
+  paper: {
+    // padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    maxHeight: '20vw',
+    overflow: 'auto',
+  },
+  button: {
+    margin: theme.spacing.unit,
+  },
+});
+
+class GlobalStateTab extends React.Component {
+    state = {
+        globalVars: [0],
+        constructorVars: [0]
+    }
+
+  render() {
+    const { classes, theme } = this.props;
+
+    return (
+      <Grid container spacing={24}>
+        <Grid item xs={6}>
+        <VariableList header={"Global State Objects"} isInput={false}/>
+        </Grid>
+        <Grid item xs={6}>
+        <VariableList header={"Constructor Parameters"} isInput={true}/>
+        </Grid>
+      </Grid>
+    );
+  }
+}
+
+GlobalStateTab.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired
+};
+
+export default withStyles(styles, { withTheme: true })(GlobalStateTab);
--- app/components/build/BuildTabs.js
+++ app/components/build/BuildTabs.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  withStyles
-} from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
@@ -10,166 +8,123 @@ import Typography from '@material-ui/core/Typography';
 import BuildDiagram from './BuildDiagram';
 import RequiresList from './RequiresList';
 import VariableList from './VariableList';
+import VariableBox from './VariableBox';
 import Grid from '@material-ui/core/Grid';
 
 function TabContainer(props) {
-  return ( <
-    Typography component = "div"
-    style = {
-      {
-        padding: 8 * 3
-      }
+    return (
+    < Typography component = "div" style = {
+        {
+            padding: 8 * 3
+        }
     } > {
-      props.children
-    } <
-    /Typography>
-  );
+        props.children
+    } < /Typography>
+    );
 }
 
 TabContainer.propTypes = {
-  children: PropTypes.node.isRequired,
+    children: PropTypes.node.isRequired,
 };
 
 const styles = theme => ({
-  tabs: {
-    flexGrow: 1,
-    width: '97%',
-    backgroundColor: theme.palette.background.paper,
-  },
+    tabs: {
+        flexGrow: 1,
+        width: '97%',
+        backgroundColor: theme.palette.background.paper,
+    },
 });
 
 class BuildTabs extends React.Component {
-    state = {
-      value: 0,
-    };
+        state = {
+            value: 0,
+        };
 
-    handleChange = (event, value) => {
-      this.setState({
-        value
-      });
-    };
+        handleChange = (event, value) => {
+            this.setState({
+                value
+            });
+        };
 
-    render() {
-      const {
-        classes
-      } = this.props;
-      const {
-        value
-      } = this.state;
+        render() {
+            const {
+                classes
+            } = this.props;
+            const {
+                value
+            } = this.state;
 
-      return ( <
-        div className = {
-          classes.tabs
-        } >
-        <
-        AppBar position = "static"
-        color = "default" >
-        <
-        Tabs value = {
-          value
-        }
-        onChange = {
-          this.handleChange
-        }
-        indicatorColor = "primary"
-        textColor = "primary"
-        scrollable scrollButtons = "auto" >
-        <
-        Tab label = "Initial State" / >
-        <
-        Tab label = "Delegate" / >
-        <
-        Tab label = "Vote" / >
-        <
-        Tab label = "Winning Proposal" / >
-        <
-        Tab label = "Winner Name" / >
-        <
-        Tab label = "+" / >
-        <
-        /Tabs> < /
-        AppBar > {
-          value === 0 &&
-          <
-          TabContainer >
-          <
-          Grid container spacing = {
+            return ( <
+                div className = {
+                    classes.tabs
+                } >
+                <
+                AppBar position = "static"
+                color = "default" >
+                <
+                Tabs value = {
+                    value
+                }
+                onChange = {
+                    this.handleChange
+                }
+                indicatorColor = "primary"
+                textColor = "primary"
+                scrollable scrollButtons = "auto" >
+                <
+                Tab label = "Initial State" / > < Tab label = "Delegate" / > < Tab label = "Vote" / > < Tab label = "Winning Proposal" / > < Tab label = "Winner Name" / > < Tab label = "+" / > < /Tabs> < /AppBar > {
+        value === 0 && < TabContainer > < Grid container spacing = {
             24
-          } >
-          <
-          Grid item xs = {
+        } > < Grid item xs = {
             6
-          } >
-          <
-          VariableList header = {
-            "Global State Objects"
-          }
-          isInput = {
-            false
-          }
-          /> < /
-          Grid > <
-          Grid item xs = {
+        } > < VariableBox header = {
+            "Variables"
+        }
+        // VariableList header = {   "Global State Objects" } isInput = {   false }
+        /> < /Grid > < Grid item xs = {
             6
-          } >
-          <
-          VariableList header = {
+        } > <
+        // VariableList header = {   "Constructor Parameters" } isInput = {   true }
+        VariableBox header = {
             "Constructor Parameters"
-          }
-          isInput = {
-            true
-          }
-          /> < /
-          Grid > <
-          /Grid> <
-          br / >
-          <
-          RequiresList header = {
+        } /> < /
+                    Grid > <
+                    /Grid > < br / > < RequiresList header = {
             "Checking Phase"
-          }
-          vars = {
+        }
+        vars = {
             ["var1", "var2"]
-          }
-          /> <
-          br / >
-          <
-          BuildDiagram / >
-          <
-          /TabContainer>} {
-          value === 1 && < TabContainer >
-          <
-          VariableList header = {
-            "Function Parameters"
-          }
-          isInput = {
-            true
-          }
-          /> <
-          br / >
-          <
-          RequiresList header = {
+        } /> < br / > < BuildDiagram / > < /TabContainer>} {
+                    value === 1 && < TabContainer >
+                    <
+                    // VariableList header = {
+                    //   "Function Parameters"
+                    // }
+                    // isInput = {
+                    //   true
+                    // }
+                    VariableBox header = {
+                        "Function Parameters"
+                    }
+                    / > < br / > < RequiresList header = {
             "Checking Phase"
-          }
-          vars = {
+        }
+        vars = {
             ["var1", "var2"]
-          }
-          /> <
-          br / >
-          <
-          BuildDiagram / >
-          <
-          /TabContainer>} {
-          value === 2 && < TabContainer > vote < /TabContainer>} {
-          value === 3 && < TabContainer > winningProposal < /TabContainer>} {
-          value === 4 && < TabContainer > winnerName < /TabContainer>} {
-          value === 5 && < TabContainer > + < /TabContainer>} < /
-          div >
-        );
-      }
+        } /> < br / > < BuildDiagram / > < /TabContainer>} {
+                    value === 2 && < TabContainer > vote < /TabContainer >
     }
+    {
+        value === 3 && < TabContainer > winningProposal < /TabContainer>} {
+                    value === 4 && < TabContainer > winnerName < /TabContainer >
+    }
+    {
+        value === 5 && < TabContainer > + < /TabContainer>} < /div >);
+    }
+}
 
-    BuildTabs.propTypes = {
-      classes: PropTypes.object.isRequired,
-    };
+BuildTabs.propTypes = {
+    classes: PropTypes.object.isRequired
+};
 
-    export default withStyles(styles)(BuildTabs);
+export default withStyles(styles)(BuildTabs);
