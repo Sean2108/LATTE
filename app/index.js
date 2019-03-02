2aa15bb89ebec2daca27d543e5e200b7d971e164
Sean2108 <sean2108@gmail.com>
2019-03-03 06:12:46 +0800

    remove unused files

--- app/index.js
+++ app/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import { configureStore, history } from './store/configureStore';
 import './app.global.css';
 import MenuAppBar from './components/MenuAppBar';
 import 'typeface-roboto';
-
-const store = configureStore();
 
 render(<MenuAppBar />, document.getElementById('app-bar'));
